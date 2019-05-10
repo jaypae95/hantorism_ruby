87a1cf66b9af7091836602acb9e76b2c18e73db7
jaypae95 <jaypae95@hbsmith.io>
2019-05-11 03:32:46 +0900

    resources posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,23 @@
 class PostsController < ApplicationController
   def index
   end
+
+  def show
+  end
+
+  def new
+  end
+
+  def predictcreate
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def post_params
+  end
 end
